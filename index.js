b88738858d994e737928f4f55557ad80a1239f11
17-321K <9951130-17-321K@users.noreply.replit.com>
2023-07-03 14:52:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+
+// 1. Make a Node 
+
+class Node {
+  constructor(value) {
+    this.value = value,
+      this.next = null
+  }
+}
+
+// 2. Create Linked List
+
+class LinkedList {
+  constructor() {
+    this.head = null,
+    this.size = 0
+  }
+  isEmpty() {
+    if(this.size === 0) return true;
+  }
+  getSize() {
+    return this.size;
+  }
+  insertNode(value) {
+    const node = new Node(value);
+    if(!this.head) {
+      this.head = node;
+    }else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+  getValue() {
+    let curr = this.head;
+    while(curr) {
+      console.log("values : ",curr.value);
+      curr = curr.next;
+    }
+  }
+}
+
+// create a New List and pass the values and test
+
+const list = new LinkedList();
+console.log("is Empty? ",list.isEmpty());
+console.log("What is Size? ",list.getSize());
+list.insertNode(10);
+list.insertNode(20);
+list.insertNode(30);
+list.getValue()
+console.log("What is Size? ",list.getSize());
